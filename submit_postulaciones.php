f0010d7a8f080e16a581e698939a5fd85236eb5e
josep <jlpaukner@gmail.com>
2022-01-24 18:39:46 -0300

    postulaciones , validacion para postulacion ya existente

--- submit_postulaciones.php
+++ submit_postulaciones.php
@@ -1,7 +1,18 @@
 <?php
 require_once('./dbcon.php');
-echo $puesto = $_POST['puesto'];
-echo $dni = $_POST['dni'];
-echo $time = $_POST['time'];
-echo $estado = $_POST['estado'];
+echo "POST";
+var_dump($_POST); 
+$DNI=$_POST['DNI'];
+$id_puesto=$_POST['id_puesto'];
+$tabla = "postulaciones";
+$nombrellave="id_postulacion";
+$valorllave="0";   
+$consulta="SELECT DNI FROM postulaciones WHERE DNI='$DNI'and id_puesto='$id_puesto'";
+$filas=cunsultadbmultiple($consulta);
+
+if(count($filas)>0) header("Location:postulaciones.php?ms=nan");
+else{
+$retorno="postulaciones.php?ms=ex";
+EnviarFormulario($tabla,$nombrellave,$valorllave,$retorno,$_POST); 
+}
 ?>
